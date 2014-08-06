1dcb8b1d233cea9a2f5f86f3b54e70fc1b0f9d77
Victor Jiang <victor.jiang@apprise.com>
2014-08-06 10:07:04 +0800

    Properties change

--- CoverGrabber/Properties/AssemblyInfo.cs
+++ CoverGrabber/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CoverGrabber")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("EraserKing")]
 [assembly: AssemblyProduct("CoverGrabber")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
